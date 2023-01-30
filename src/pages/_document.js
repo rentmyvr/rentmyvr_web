51bd081bd634970b0a2dde5d6696fabb3f52e065
Paul Okeke <pauldiconline@gmail.com>
2023-01-30 20:16:45 +0100

    Fixed FavIcon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         <meta name="description" content="DESCRIPTION_CONTENT" />
         <meta name="keywords" content="KEYWORDS_CONTENT" />
 
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/assets/images/icon.svg" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap&family=Public+Sans:wght@400;500;600;700"
