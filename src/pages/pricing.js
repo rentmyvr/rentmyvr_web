ca926521b5b6651004371ec4f39dd452079d2b08
hackykitty <talulawilder1@gmail.com>
2023-03-10 18:25:25 -0800

    fix: rearrange all component in pricing page

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -15,7 +15,9 @@ import {
   TableCell,
   TableBody,
   Paper,
-  Link
+  Link,
+  styled,
+  tableCellClasses
 } from '@mui/material';
 
 // project imports
@@ -26,7 +28,6 @@ import NextLink from 'next/link';
 
 // assets
 import { UpOutlined } from '@ant-design/icons';
-import { useState } from 'react';
 
 const Pricing = () => {
   const trigger = useScrollTrigger({ disableHysteresis: true, threshold: 800 });
@@ -42,119 +43,41 @@ const Pricing = () => {
     return { name, standard, premium };
   }
 
-  const [standardFree, setStandardFree] = useState(true);
-  const [standardMainImage, setStandardMainImage] = useState(true);
-  const [standardHostDashboard, setStandardHostDashboard] = useState(true);
-  const [standardPropertyBasics, setStandardPropertyBasics] = useState(true);
-  const [standardCalendarSynch, setStandardCalendarSynch] = useState(true);
-  const [standardLinkSocial, setStandardLinkSocial] = useState(true);
-  const [standardLinkBooking, setStandardLinkBooking] = useState(true);
-  const [standardPreferred, setStandardPreferred] = useState(false);
-  const [standardPropertyDesc, setStandardPropertyDesc] = useState(false);
-  const [standardUltimateInquiry, setStandardUltimateInquiry] = useState(false);
-  const [standardUltimateImage, setStandardUltimateImage] = useState(false);
-  const [standardLinkVideo, setStandardLinkVideo] = useState(false);
-  const [standardAdvancedAmenties, setStandardAdvancedAmenties] = useState(false);
-  const [standardAdvancedSearch, setStandardAdvancedSearch] = useState(false);
-
-  const [premiumFree, setPremiumFree] = useState(true);
-  const [premiumMainImage, setPremiumMainImage] = useState(true);
-  const [premiumHostDashboard, setPremiumHostDashboard] = useState(true);
-  const [premiumPropertyBasics, setPremiumPropertyBasics] = useState(true);
-  const [premiumCalendarSynch, setPremiumCalendarSynch] = useState(true);
-  const [premiumLinkSocial, setPremiumLinkSocial] = useState(true);
-  const [premiumLinkBooking, setPremiumLinkBooking] = useState(true);
-  const [premiumPreferred, setPremiumPreferred] = useState(true);
-  const [premiumPropertyDesc, setPremiumPropertyDesc] = useState(true);
-  const [premiumUltimateInquiry, setPremiumUltimateInquiry] = useState(true);
-  const [premiumUltimateImage, setPremiumUltimateImage] = useState(true);
-  const [premiumLinkVideo, setPremiumLinkVideo] = useState(true);
-  const [premiumAdvancedAmenties, setPremiumAdvancedAmenties] = useState(true);
-  const [premiumAdvancedSearch, setPremiumAdvancedSearch] = useState(true);
-
-  const standardChange = (name) => {
-    switch (name) {
-      case 'Free Support':
-        return setStandardFree(!standardFree);
-      case 'Main Image Upload':
-        return setStandardMainImage(!standardMainImage);
-      case 'Host Dashboard':
-        return setStandardHostDashboard(!standardHostDashboard);
-      case 'Property Basics':
-        return setStandardPropertyBasics(!standardPropertyBasics);
-      case 'Calendar Synchronization':
-        return setStandardCalendarSynch(!standardCalendarSynch);
-      case "Link to Property's Social Sites":
-        return setStandardLinkSocial(!standardLinkSocial);
-      case 'Link to All Property Booking Sites':
-        return setStandardLinkBooking(!standardLinkBooking);
-      case 'Preferred Listing Placement':
-        return setStandardPreferred(!standardPreferred);
-      case 'Property Description':
-        return setStandardPropertyDesc(!standardPropertyDesc);
-      case 'Ultimated Inquiries/ Leads through Contact Button':
-        return setStandardUltimateInquiry(!standardUltimateInquiry);
-      case 'Ultimated Images':
-        return setStandardUltimateImage(!standardUltimateImage);
-      case 'Link to Video or Virtual Tour':
-        return setStandardLinkVideo(!standardLinkVideo);
-      case 'Advanced Amenities and Categories':
-        return setStandardAdvancedAmenties(!standardAdvancedAmenties);
-      case 'Advanced Search Functionality':
-        return setStandardAdvancedSearch(!standardAdvancedSearch);
-      default:
-        return null;
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: '#1890ff',
+      color: theme.palette.common.white
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14
     }
-  };
-  const premiumChange = (name) => {
-    switch (name) {
-      case 'Free Support':
-        return setPremiumFree(!premiumFree);
-      case 'Main Image Upload':
-        return setPremiumMainImage(!premiumMainImage);
-      case 'Host Dashboard':
-        return setPremiumHostDashboard(!premiumHostDashboard);
-      case 'Property Basics':
-        return setPremiumPropertyBasics(!premiumPropertyBasics);
-      case 'Calendar Synchronization':
-        return setPremiumCalendarSynch(!premiumCalendarSynch);
-      case "Link to Property's Social Sites":
-        return setPremiumLinkSocial(!premiumLinkSocial);
-      case 'Link to All Property Booking Sites':
-        return setPremiumLinkBooking(!premiumLinkBooking);
-      case 'Preferred Listing Placement':
-        return setPremiumPreferred(!premiumPreferred);
-      case 'Property Description':
-        return setPremiumPropertyDesc(!premiumPropertyDesc);
-      case 'Ultimated Inquiries/ Leads through Contact Button':
-        return setPremiumUltimateInquiry(!premiumUltimateInquiry);
-      case 'Ultimated Images':
-        return setPremiumUltimateImage(!premiumUltimateImage);
-      case 'Link to Video or Virtual Tour':
-        return setPremiumLinkVideo(!premiumLinkVideo);
-      case 'Advanced Amenities and Categories':
-        return setPremiumAdvancedAmenties(!premiumAdvancedAmenties);
-      case 'Advanced Search Functionality':
-        return setPremiumAdvancedSearch(!premiumAdvancedSearch);
-      default:
-        return null;
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0
     }
-  };
+  }));
+
   const rows = [
-    createData('Free Support', standardFree, premiumFree),
-    createData('Main Image Upload', standardMainImage, premiumMainImage),
-    createData('Host Dashboard', standardHostDashboard, premiumHostDashboard),
-    createData('Property Basics', standardPropertyBasics, premiumPropertyBasics),
-    createData('Calendar Synchronization', standardCalendarSynch, premiumCalendarSynch),
-    createData("Link to Property's Social Sites", standardLinkSocial, premiumLinkSocial),
-    createData('Link to All Property Booking Sites', standardLinkBooking, premiumLinkBooking),
-    createData('Preferred Listing Placement', standardPreferred, premiumPreferred),
-    createData('Property Description', standardPropertyDesc, premiumPropertyDesc),
-    createData('Ultimated Inquiries/ Leads through Contact Button', standardUltimateInquiry, premiumUltimateInquiry),
-    createData('Ultimated Images', standardUltimateImage, premiumUltimateImage),
-    createData('Link to Video or Virtual Tour', standardLinkVideo, premiumLinkVideo),
-    createData('Advanced Amenities and Categories', standardAdvancedAmenties, premiumAdvancedAmenties),
-    createData('Advanced Search Functionality', standardAdvancedSearch, premiumAdvancedSearch)
+    createData('Free Support', true, true),
+    createData('Main Image Upload', true, true),
+    createData('Host Dashboard', true, true),
+    createData('Property Basics', true, true),
+    createData('Calendar Synchronization', true, true),
+    createData("Link to Property's Social Sites", false, true),
+    createData('Link to All Property Booking Sites', false, true),
+    createData('Preferred Listing Placement', false, true),
+    createData('Property Description', false, true),
+    createData('Ultimited Inquiries/ Leads through Contact Button', false, true),
+    createData('Ultimited Images', false, true),
+    createData('Link to Video or Virtual Tour', false, true),
+    createData('Advanced Amenities and Categories', false, true),
+    createData('Advanced Search Functionality', false, true)
   ];
 
   return (
@@ -164,11 +87,15 @@ const Pricing = () => {
           <Grid item style={{ width: '100%' }}>
             <img style={{ width: '100%' }} src="/assets/images/pricing.png" alt="" />
           </Grid>
-          <Grid item sm={10} md={8} xs={10} my={5}>
-            <Typography my={1}>
-              Sign up to create an account for free. List an unlimited number of standard property listings for free -
+          <Grid item sm={10} md={8} xs={10} mt={5} ml={2}>
+            <Typography mb={5} variant="h4" style={{ textAlign: 'center' }}>
+              Whether you are looking to drive more traffic to your management company or your property listing, check out our flex pricing
+              module below.
             </Typography>
-            <Typography>- No hidden fees, booking commissions, or fees to the host or the guest.</Typography>
+            <Typography mb={1}>
+              Sign up to create an account for free. List an unlimited number of standard property listings for free
+            </Typography>
+            <Typography mb={1}> No hidden fees, booking commissions, or fees to the host or the guest.</Typography>
             <Typography mb={1}>
               Boost your listing by upgrading to a premium property listing, and choose the pricing module that best suits you.
             </Typography>
@@ -176,72 +103,82 @@ const Pricing = () => {
               Whether you are looking to drive more traffic to your management company or your property listing, check out our flex pricing
               module below.
             </Typography>
-            <Box mb={1}>
-              List your company/{' '}
-              <Box fontWeight="800" display="inline">
-                One time fee - $159 per year
-              </Box>
-            </Box>
-            <Box mb={1}>
-              List a property/{' '}
-              <Box fontWeight="800" display="inline">
-                $129 per year flat fee OR $29 monthly option
-              </Box>
-            </Box>
-            <Box mb={1}>
-              Our team can input your listing for you for a one time setup charge/{' '}
-              <Box fontWeight="800" display="inline">
-                $39 per property
-              </Box>
-            </Box>
+            <ul style={{ fontSize: '1.1rem' }}>
+              <li>
+                <Box>
+                  List your company/{' '}
+                  <Box fontWeight="800" display="inline">
+                    One time fee - $159 per year
+                  </Box>
+                </Box>
+              </li>
+              <li>
+                <Box>
+                  List a property/{' '}
+                  <Box fontWeight="800" display="inline">
+                    $129 per year flat fee OR $29 monthly option
+                  </Box>
+                </Box>
+              </li>
+              <li>
+                <Box>
+                  Our team can input your listing for you for a one time setup charge/{' '}
+                  <Box fontWeight="800" display="inline">
+                    $39 per property
+                  </Box>
+                </Box>
+              </li>
+            </ul>
           </Grid>
-          <Grid item sm={12} md={8} mb={2}>
+          <Grid item style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} my={3}>
+            <Typography mb={2} variant="h4">
+              Create an Account and Start Listing!
+            </Typography>
+            <NextLink href="/register" passHref>
+              <Link className="header-link" color="black" underline="none">
+                <Button size="large" variant="contained" color="primary">
+                  Get Started
+                </Button>
+              </Link>
+            </NextLink>
+          </Grid>
+          <Grid item sm={12} md={8} mb={2} xs={12} ml={3} mr={1}>
             <TableContainer component={Paper}>
               <Table aria-label="simple table">
                 <TableHead>
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell align="right" style={{ textTransform: 'none' }}>
+                  <StyledTableRow>
+                    <StyledTableCell></StyledTableCell>
+                    <StyledTableCell align="right" style={{ textTransform: 'none', fontSize: '0.9rem' }}>
                       Standard Listing
-                    </TableCell>
-                    <TableCell align="right" style={{ textTransform: 'none' }}>
+                    </StyledTableCell>
+                    <StyledTableCell align="right" style={{ textTransform: 'none', fontSize: '0.9rem' }}>
                       Premium Listing
-                    </TableCell>
-                  </TableRow>
+                    </StyledTableCell>
+                  </StyledTableRow>
                 </TableHead>
                 <TableBody>
                   {rows.map((row) => (
-                    <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <StyledTableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                       <TableCell component="th" scope="row">
                         {row.name}
                       </TableCell>
-                      <TableCell align="right">
-                        <input
-                          type="checkbox"
-                          style={{ width: '16px', height: '16px' }}
-                          checked={row.standard}
-                          onChange={() => {
-                            standardChange(row.name);
-                          }}
-                        />
+                      <TableCell align="center">
+                        {(row.standard && <img src="https://thecustomizewindows.com/Downloads/Media/16.png" alt="" />) || (
+                          <img src="https://thecustomizewindows.com/Downloads/Media/17.png" alt="" />
+                        )}
                       </TableCell>
-                      <TableCell align="right">
-                        <input
-                          type="checkbox"
-                          style={{ width: '16px', height: '16px' }}
-                          checked={row.premium}
-                          onChange={() => {
-                            premiumChange(row.name);
-                          }}
-                        />
+                      <TableCell align="center">
+                        {(row.premium && <img src="https://thecustomizewindows.com/Downloads/Media/16.png" alt="" />) || (
+                          <img src="https://thecustomizewindows.com/Downloads/Media/17.png" alt="" />
+                        )}
                       </TableCell>
-                    </TableRow>
+                    </StyledTableRow>
                   ))}
                 </TableBody>
               </Table>
             </TableContainer>
           </Grid>
-          <Grid item sm={12} md={8} mb={2}>
+          <Grid item sm={9} md={8} xs={10} mb={2} ml={2}>
             <Typography mb={1} variant="h5">
               Looking to list in bulk?
             </Typography>
@@ -250,7 +187,7 @@ const Pricing = () => {
               <li>$79 per property per year when listing 50 or more properties</li>
             </ul>
           </Grid>
-          <Grid item sm={12} md={8} mb={2}>
+          <Grid item sm={12} md={8} mb={3}>
             <Grid item style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
               <Typography mb={2} variant="h4">
                 Have a question about Pricing?
@@ -259,18 +196,6 @@ const Pricing = () => {
                 <Link className="header-link" color="black" underline="none">
                   <Button size="large" variant="contained" color="primary">
                     Contact Us
-                  </Button>
-                </Link>
-              </NextLink>
-            </Grid>
-            <Grid item style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} my={5}>
-              <Typography mb={2} variant="h4">
-                Create an Account and Start Listing!
-              </Typography>
-              <NextLink href="/register" passHref>
-                <Link className="header-link" color="black" underline="none">
-                  <Button size="large" variant="contained" color="primary">
-                    Get Started
                   </Button>
                 </Link>
               </NextLink>
